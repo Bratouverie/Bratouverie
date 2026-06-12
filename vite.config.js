63bae3bdcdc27491f92001506f258f7f773d9d72
Bratouverie <109337825+Bratouverie@users.noreply.github.com>
2026-06-12 23:36:19 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Kontrack2026/',
+  base: '/', '/Kontrack2026/',
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
